f90dfc59a8e3fb70b6aee6c77f1c9aa1dd222cd3
thecmart <conrad@chatsdk.co>
2019-03-14 13:42:48 +0100

    First commit

---
+++ app/src/main/java/co/chatsdk/android/app/CameraButton.java
@@ -0,0 +1,38 @@
+package co.chatsdk.android.app;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class CameraButton extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camerabutton);
+
+        Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CameraButton.this, CameraActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+    }
+}
